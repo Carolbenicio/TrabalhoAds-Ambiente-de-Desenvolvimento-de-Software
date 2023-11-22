b268b5775c54234506b365173bd8d16002f94644
Caroline Benicio <144948428+Carolbenicio@users.noreply.github.com>
2023-11-22 17:52:28 -0300

    Criação da classe Veiculo

---
+++ Veiculo.java
@@ -0,0 +1,14 @@
+public class Veiculo {
+    private String marca;
+    private String modelo;
+    private String cor;
+
+
+    
+   public Veiculo(String marca, String modelo, String Cor) {
+    this.marca = marca;
+    this.modelo = modelo;
+    this.cor = cor;
+   }
+   
+}
